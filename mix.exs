8b76133454b072ccb01f9bbefce19ba15c85bd50
Mayank Tripathi <ryotsu@kochika.me>
2017-05-16 18:42:32 +0530

    Setup Gitlab CI

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule EmailUpload.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+      {:credo, "~> 0.7", only: [:dev, :test]},
+      {:dogma, "~> 0.1", only: [:dev, :test]},
+    ]
   end
 end
